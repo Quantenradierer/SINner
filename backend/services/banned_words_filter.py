30c19aecbd8a1028b3d675f11f8356192f999006
Leto <leto+git@quantenradierer.de>
2023-03-09 23:46:46 +0100

    fix: only block banned words, not banned parts (like 'ass' in 'glasses')

--- backend/services/banned_words_filter.py
+++ backend/services/banned_words_filter.py
@@ -5,14 +5,13 @@ from functools import cache
 def banned_words():
     with open('data/midjourney_banned_words.txt') as file:
         banned_words = file.readlines()
-    return [word.strip().lower() for word in banned_words if word.strip()]
+    return [' ' + word.strip().lower() + ' ' for word in banned_words if word.strip()]
 
 
 def contains_banned_word(text):
     text = text.lower()
     for word in banned_words():
         if word in text:
-            print(word)
             return True
     return False
 
