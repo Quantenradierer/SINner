a262bea7f2e1cec7592e15a2f0661fc3abe4be8b
Leto <leto+git@quantenradierer.de>
2023-03-20 17:33:00 +0100

    fix: adds settings for django for docker production deployment

--- dockerscript.sh
+++ dockerscript.sh
@@ -2,6 +2,6 @@ source .env
 cd ./frontend/
 npm run build
 cd ..
-docker build -f Dockerfile -t sinner . --build-arg OPENAI_API_KEY=$OPENAI_API_KEY --build-arg MJ_CHANNEL_ID=$MJ_CHANNEL_ID --build-arg MJ_PRIVATE_DISCORD_TOKEN=$MJ_PRIVATE_DISCORD_TOKEN --build-arg MJ_SERVER_ID=$MJ_SERVER_ID
+docker build -f Dockerfile -t sinner . --build-arg OPENAI_API_KEY=$OPENAI_API_KEY --build-arg MJ_CHANNEL_ID=$MJ_CHANNEL_ID --build-arg MJ_PRIVATE_DISCORD_TOKEN=$MJ_PRIVATE_DISCORD_TOKEN --build-arg MJ_SERVER_ID=$MJ_SERVER_ID --build-arg DJANGO_SECURE_KEY=$DJANGO_SECURE_KEY --build-arg DJANGO_ALLOWED_HOST=$DJANGO_ALLOWED_HOST
 docker stop sinner
 docker run -p 8000:80 --mount type=volume,src=sinner-data,target=/app/data --name sinner --rm -it sinner
