0e62fd82756815191f85b678b40e3168390143cd
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-04-06 01:39:51 +0200

    fix: install npm package before building

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,5 +1,6 @@
 source .env
 cd ./frontend/
+npm install
 npm run build
 cd ..
 docker build -f Dockerfile -t sinner . --build-arg OPENAI_API_KEY=$OPENAI_API_KEY --build-arg MJ_CHANNEL_ID=$MJ_CHANNEL_ID --build-arg MJ_PRIVATE_DISCORD_TOKEN=$MJ_PRIVATE_DISCORD_TOKEN --build-arg MJ_SERVER_ID=$MJ_SERVER_ID --build-arg DJANGO_SECRET_KEY=$DJANGO_SECRET_KEY --build-arg DJANGO_HOST=$DJANGO_HOST --build-arg DJANGO_PORT=$DJANGO_PORT
