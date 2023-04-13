7dd8514e86eb915f63e9fd34d3a70a9884b7a7e1
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-04-13 13:27:40 +0200

    fix: change docker port

--- dockerscript.sh
+++ dockerscript.sh
@@ -4,4 +4,4 @@ npm run build
 cd ..
 docker build -f Dockerfile -t sinner .
 docker stop sinner
-docker run -p 8000:80 --mount type=volume,src=sinner-data,target=/app/data --name sinner --rm -it sinner
+docker run -p 4117:80 --mount type=volume,src=sinner-data,target=/app/data --name sinner --rm -it sinner
