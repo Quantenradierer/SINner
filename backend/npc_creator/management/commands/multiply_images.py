46e2873f533e93968c895535df07cf98faf7ebd0
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-04-23 20:28:42 +0200

    fix: adds migration

--- backend/npc_creator/management/commands/multiply_images.py
+++ backend/npc_creator/management/commands/multiply_images.py
@@ -9,18 +9,16 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, **options):
-        amount = 2 + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1)
+        amount = 2 + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1)
 
-        for i in range(2):
+        for i in range(amount):
             #male_template = example_image.find('troll_male')
-            prompt = f'In Shadowrun, a female troll' # {male_template.url}
+            prompt = f'In Shadowrun/Cyberpunk: A 38-year-old of English background ethnicity. An orc who works as an illegal fighting organizer. The orc named "Bonebreaker" is 1.90 meters tall and weighs just under 140 kg, with most of his mass consisting of muscle. His skull is noticeably larger than the normal human skull, and he has dense black hair, which he usually wears in a frayed mohawk. He has a scar on his left eye and his skin is covered in numerous tattoos.'
+            # {male_template.url}
 
-            prompt = special_midjourney_prompt(prompt, metatyp='Troll', gender='weiblich')
+            prompt = special_midjourney_prompt(prompt, metatyp='Ork', gender='männlich')
             pass_prompt(prompt)
             print(prompt)
-            sleeptime = random.randint(0, 100) / 100 + 2 + random.randint(0, 3) + random.randint(0, 3) + random.randint(0, 3) + random.randint(0, 2) + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1)
-            #print(sleeptime)
-            time.sleep(sleeptime)
-            # minotaur:: fur::-0.1 animal::-0.1 monster::-0.1
-            # female troll:: female:: minotaur:: wrinkles:: small horns:: fur::-0.3 beard::-0.3
 
+            sleeptime = random.randint(0, 100) / 100 + 2 + random.randint(0, 3) + random.randint(0, 3) + random.randint(0, 3) + random.randint(0, 2) + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1) + random.randint(0, 1)
+            time.sleep(sleeptime)
