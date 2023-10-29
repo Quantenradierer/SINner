e8dc2a75ef9d7c9240e631ad9fe6cedceaef0339
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-10-29 14:48:36 +0100

    feature: for deployment, wait for docker to stop

--- dockerscript.sh
+++ dockerscript.sh
@@ -5,4 +5,5 @@ cd ..
 sudo cp /var/lib/docker/volumes/sinner-data/_data/db.sqlite3 ../backups/db_$(date +%Y-%m-%d_%H%M).sqlite
 docker build -f Dockerfile -t sinner .
 docker stop sinner
+docker wait sinner
 docker run -p 4117:80 --mount type=volume,src=sinner-data,target=/app/data --name sinner --rm -it sinner
