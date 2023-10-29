d4e36954bfccabbbff978ac4be9c9ee674c69c42
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-10-29 14:26:35 +0100

    feature: changes troll appearances

--- backend/npc_creator/services/special_midjourney_prompt.py
+++ backend/npc_creator/services/special_midjourney_prompt.py
@@ -32,8 +32,8 @@ def transform_gender(gender):
 
 def get_multiprompts(metatyp_gender):
     return {
-        'troll_male': 'small horns:: minotaur::0.8 bull::-0.1 animal::-0.1 ',
-        'troll_female': 'female::2 suit:: minotaur::1.5 small horns:: wrinkles:: fur::-0.5 beard::-0.5 cyberpunk::',
+        'troll_male': 'small horns:: minotaur::0.8 clothes:: cyberpunk:: bull::-0.1 animal::-0.1 ',
+        'troll_female': 'female::2 suit:: minotaur::1.5 small horns:: wrinkles:: clothes:: cyberpunk:: fur::-0.5 beard::-0.5 cyberpunk::',
         'orc_male': 'green skin::-0.3 wears jacket, suit or hoodie:: human with orc tusks:: armor::-0.1 ',
         'orc_female': 'green skin::-0.3 wears jacket, suit or hoodie:: human with orc tusks:: armor::-0.1 ',
         'dwarf_male': 'dwarf:: gnome::-0.2 elf::-0.2',
