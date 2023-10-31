1c21289c547982b66a3ffc8baf9ce5cfc7395e0f
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-10-31 19:52:48 +0100

    refactor: javascript code style

--- frontend/src/loader/npc_list_loader.js
+++ frontend/src/loader/npc_list_loader.js
@@ -4,7 +4,7 @@ import api from "../axios";
 export async function npcListLoader({ request }) {
     let url = new URL(request.url);
     try {
-        const response = await api.get('/api/npc_creator/npcs/' + url.search)
+        const response = await api.get(`/api/npc_creator/npcs/${url.search}`)
         return response.data
     } catch (error) {
         return null
