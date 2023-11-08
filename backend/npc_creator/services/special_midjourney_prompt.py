2d2791b995235dba2f86e82e11af8abc3fd9b32e
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-11-09 00:23:56 +0100

    fix: unify image additions

--- backend/npc_creator/services/special_midjourney_prompt.py
+++ backend/npc_creator/services/special_midjourney_prompt.py
@@ -34,8 +34,8 @@ def get_multiprompts(metatyp_gender):
     return {
         'troll_male': 'small horns:: minotaur::0.8 clothes:: cyberpunk::0.3 bull::-0.1 animal::-0.1 ',
         'troll_female': 'female::2 suit:: minotaur::1.5 small horns:: wrinkles:: clothes:: cyberpunk::0.3 fur::-0.5 beard::-0.5 cyberpunk::',
-        'orc_male': 'green skin::-0.3 wears jacket, suit or hoodie:: cyberpunk::0.3 human with orc tusks:: armor::-0.1 ',
-        'orc_female': 'green skin::-0.3 wears jacket, suit or hoodie:: cyberpunk::0.3 human with orc tusks:: armor::-0.1 ',
+        'orc_male': 'green skin::-0.3 clothes:: cyberpunk::0.3 human with orc tusks:: armor::-0.1 ',
+        'orc_female': 'green skin::-0.3 clothes:: cyberpunk::0.3 human with orc tusks:: armor::-0.1 ',
         'dwarf_male': 'cyberpunk::0.3 dwarf:: gnome::-0.2 elf::-0.2',
         'dwarf_female': 'cyberpunk::0.3 dwarf:: female:: gnome::-0.2 elf::-0.2'
     }.get(metatyp_gender, 'cyberpunk::0.3')
