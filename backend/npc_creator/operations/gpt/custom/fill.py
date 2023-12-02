3e0015cddb00fe5a5bfde8c9f77f8c11485f501f
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-12-03 00:06:24 +0100

    improve custom image creation

--- backend/npc_creator/operations/gpt/custom/fill.py
+++ backend/npc_creator/operations/gpt/custom/fill.py
@@ -8,6 +8,7 @@ class Fill(entity.Fill):
         super().__init__(**kwargs)
         self.use_json = True
         self.system_prompt = (
+            "- Du bist ein Illustrator mit 100 Jahren Erfahrung\r\n"
             "- Antworte NUR als JSON in dem Format {'Aussehen': 'Eine gute Beschreibung für einen Bildgenerator'}\r\n"
             "- Das JSON darf nur diesen einen String und keine weiteren Objects/Vertiefungen enthalten\r\n"
             "- Frage nicht nach Details, sondern denke dir selbst Details aus\r\n"
