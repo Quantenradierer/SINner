142ebbd6809bdc5fedd31badf2925bae987ea59e
Bernd Schmidt <schmidt.bernd.1989@gmail.com>
2023-12-15 01:58:10 +0100

    fix auto refresh after 5 seconds

--- frontend/src/components/use_refresh_entity_card.js
+++ frontend/src/components/use_refresh_entity_card.js
@@ -3,20 +3,24 @@ import { useEffect, useState } from 'react';
 import EntityLoader from "../loader/entity_loader";
 
 const useRefreshEntityCard = (entityType, entity, setEntity, interval = 5000) => {
-  useEffect(() => {
-    const fetchData = async () => {
-      let newEntity = await new EntityLoader(entityType).entity({'params': {'id': entity.id}, undefined})
+    useEffect(() => {
+      const fetchData = async () => {
+        let newEntity = await new EntityLoader(entityType).entity({'params': {'id': entity.id}, undefined})
 
-      if (newEntity.image_objects.length > 0) {
-        clearInterval(timer);
-        setEntity(newEntity);
+        if (newEntity.image_objects.length > 0) {
+          clearInterval(timer);
+          setEntity(newEntity);
+        }
+      };
+
+      var timer = undefined;
+      if (entity.image_objects.length == 0) {
+        const timer = setInterval(fetchData, interval);
+        return () => clearInterval(timer);
       }
-    };
+    }, []);
 
-    const timer = setInterval(fetchData, interval);
 
-    return () => clearInterval(timer);
-  }, []);
 };
 
 export default useRefreshEntityCard;
